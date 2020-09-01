1041443b6a2db07b404ddfb3912a25352f2d02b6
alpha-martinez <martinezalphaa@gmail.com>
2020-09-01 19:19:02 -0400

    added the get and post routes. hopefully this works

--- controllers/pieces.js
+++ controllers/pieces.js
@@ -1,17 +1,38 @@
 // Require needed modules
 const express = require('express');
+const { db } = require('../models/piece');
 
 // Declare router
 const router = express.Router();
 
 router.get('/', (req, res) => {
   // TODO: Replace stub route with page that renders list of all pieces
-  res.render('pieces/index');
+  db.Piece.find()
+  .then(pieceArray => {
+    console.log(pieceArray)
+    res.send(pieceArray)
+  }).catch(err => {
+    console.log(err, 'error')
+    res.status(503).send({message: 'Database asleep?'})
+  })
+  // res.render('pieces/index');
 });
 
 router.post('/', (req, res) => {
   // TODO: Replace stub route with page that renders form for adding new piece
-  res.send('STUB - NEW PIECES POST');
+  db.Piece.create(req.body)
+  .then(createPiece => {
+    console.log(createPiece)
+    res.status(201).send(createPiece)
+  }).catch(err => {
+    console.log('Error while creating new piece')
+    if(err.name === 'Validation Error') {
+      res.status(406).send({message: 'Validation Error'})
+    } else {
+      res.status(503).send({message: 'Database asleep'})
+    }
+  })
+  // res.send('STUB - NEW PIECES POST');
 });
 
 router.get('/new', (req, res) => {
